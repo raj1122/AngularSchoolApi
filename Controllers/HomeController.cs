6a5e5104430e6c24477f16871e36ec036409b6b3
raj1122 <arialstarraj@gmail.com>
2019-08-12 14:17:50 +0530

    Initial Commit.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller  
+    {
+
+        
+
+    }
+    
+
+}
